acfff230a3b022cee62c5d75cd522df0d28ec739
jungbinyim81-dotcom <jungbinyim81@gmail.com>
2026-06-01 14:39:03 +0900

    team4 platform update 2026-06-01 00:00

---
+++ team4/sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "team4-20260601-1439";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon.svg",
+  "./거래처처방현황통합관리_4팀.html",
+  "./API_실적분석.html",
+  "./팀개원처현황.html",
+  "./RnP규정.html",
+  "./RnP결과.html"
+];
+
+// 설치: 최신 자료 캐싱
+self.addEventListener("install", e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS).catch(()=>{})));
+});
+
+// 활성화: 옛 캐시 삭제 (자료 갱신 즉시 반영)
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(()=> self.clients.claim())
+  );
+});
+
+// 네트워크 우선 (최신성 보장), 실패 시 캐시 (오프라인)
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    fetch(e.request).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE_NAME).then(c => c.put(e.request, copy)).catch(()=>{});
+      return res;
+    }).catch(() => caches.match(e.request))
+  );
+});
