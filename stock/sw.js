41cedd77d4c935eb9b26219a882d0b16909ab7b7
jungbinyim81-dotcom <jungbinyim81@gmail.com>
2026-06-14 05:42:33 +0900

    stock app update 2026-06-14 05:42

--- stock/sw.js
+++ stock/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "soxl-20260614-0538";
+const CACHE_NAME = "soxl-20260614-0542";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", e => {
