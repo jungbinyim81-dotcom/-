f1bc4c34bf25e6c45d62e957cc34f6b8db085e4d
jungbinyim81-dotcom <jungbinyim81@gmail.com>
2026-06-13 19:13:37 +0900

    stock app update 2026-06-13 19:13

---
+++ stock/sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = "soxl-20260613-1913";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icon.svg"];
+
+self.addEventListener("install", e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS).catch(()=>{})));
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+// data.json 은 네트워크 우선 (항상 최신), 나머지는 캐시 우선
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  const url = new URL(e.request.url);
+  if (url.pathname.endsWith('data.json')) {
+    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  } else {
+    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  }
+});
