1cee108d77f33602107f8780fa3939681777ff78
jungbinyim81-dotcom <jungbinyim81@gmail.com>
2026-08-03 08:06:15 +0900

    muscle app update 2026-08-03 00:00

--- muscle/sw.js
+++ muscle/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "muscle-20260803-0545";
+const CACHE = "muscle-20260803-0806";
 const ASSETS = ["./","./index.html","./manifest.json","./icon.svg"];
 self.addEventListener("install", e => {
   self.skipWaiting();
