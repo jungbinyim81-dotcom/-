4429949257fc052e95a54b56cc2aeb4f0aef8616
jungbinyim81-dotcom <jungbinyim81@gmail.com>
2026-08-25 05:47:53 +0900

    가계부 폰앱 20260825-0547

--- de0f39760c72/sw.js
+++ de0f39760c72/sw.js
@@ -1,4 +1,4 @@
-const C='gagyebu-20260825-0546';
+const C='gagyebu-20260825-0547';
 self.addEventListener('install',e=>{self.skipWaiting();
  e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./manifest.json','./icon.svg'])))});
 self.addEventListener('activate',e=>{e.waitUntil(
