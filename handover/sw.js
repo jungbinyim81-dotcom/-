2f1f70004bd46b7a21f500b0ea01769c54b0ace4
jungbinyim81-dotcom <jungbinyim81@gmail.com>
2026-09-26 16:14:15 +0900

    handover map 2026-09-26

--- handover/sw.js
+++ handover/sw.js
@@ -1,4 +1,4 @@
-const C='handover-20260926-1613';
+const C='handover-20260926-1614';
 const F=['./','./index.html','./manifest.json','./icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(F)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
